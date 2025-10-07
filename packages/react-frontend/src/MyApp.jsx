73f37f6533fb4239c548e358a0d579badbecb9ca
pzalan <115607499+pzalan@users.noreply.github.com>
2025-10-07 12:51:49 -0700

    modified the updatelist and can now post a user to the backend

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -11,8 +11,12 @@ function MyApp() {
     setCharacters(updated);
   }
   
-  function updateList(person){ // will update the lsit 
-    setCharacters([...characters, person]); } 
+  function updateList(person){ // will update the list only if our POST call is successful 
+     postUser(person) 
+        .then(() =>  setCharacters([...characters, person]))
+	.catch((error) => {
+	  console.log(error); })
+} 
 
   function fetchUsers(){ //this will make the GET request from the backend  
 	const promise = fetch("http://localhost:8000/users");
@@ -24,6 +28,17 @@ function MyApp() {
 	    .then((json) => setCharacters(json["users_list"])) //initializes the state of the component
 	    .catch((error) => { console.log(error); });
  }, []); //only called when the Myapp component passes an empty array as the second arg to useEffect
+  
+  function postUser(person) { // this will make a post req and return the res 
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST", //makes it a POST instead of the default GET 
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person), //converts the in memory object to a string 
+    });
+
+    return promise; }
 
   return(
     <div className="container">
